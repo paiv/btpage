cc4c9f2c360db2de636f5e181975c3e44289d0c1
Pavel Ivashkov <pavels.code@gmail.com>
2017-04-20 12:59:52 +0300

    : initial project

---
+++ btpage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  btpage
+//
+//  Created by Pavel Ivashkov on 2017-04-20.
+//  Copyright © 2017 proto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
